10325de5920deaf4bc8f302f5b2ca30b954c8e37
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2025-02-23 11:53:34 -0300

    adds

---
+++ js.js
@@ -0,0 +1,11 @@
+var compra = () => {
+    window.open("compra.html", "_blank");
+}
+
+var alugar = () => {
+    window.open("alugar.html", "_blank");
+}
+
+var alugar = () => {
+    window.open("vender.html", "_blank");
+}
